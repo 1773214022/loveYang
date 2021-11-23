5dff0f5ac5285ef3e41ad0c14d2dbb81834f6027
xausky <xausky@163.com>
2021-11-23 23:18:08 +0800

    update

--- index.js
+++ index.js
@@ -11,7 +11,9 @@ class ThreeDWorld {
         this.addObjs();
         // 轨道控制插件（鼠标拖拽视角、缩放等）
         this.orbitControls = new THREE.OrbitControls(this.camera);
-        this.cubic = BezierEasing(0.25, 0.1, 0.25, 1);
+        this.orbitControls.maxDistance = 300;
+        this.orbitControls.minDistance = 100;
+        this.cubic = BezierEasing(0.25, 0.0, 0.25, 1.0);
         // this.orbitControls.autoRotate = true;
         // 循环更新场景
         this.update();
@@ -44,7 +46,7 @@ class ThreeDWorld {
 
         // 设置相机的位置
         this.camera.position.x = 0;
-        this.camera.position.z = 100;
+        this.camera.position.z = 200;
         this.camera.position.y = 0;
         // 创建渲染器
         this.renderer = new THREE.WebGLRenderer({
@@ -87,7 +89,7 @@ class ThreeDWorld {
     addObjs() {
         var loader = new THREE.FontLoader();
         loader.load('LESLIE.json', (font) => {
-            var geometrys = new Array(10)
+            var geometrys = new Array(12)
             for(var i = 0; i < 10; i++){
                 geometrys[i] = new THREE.TextGeometry(String.fromCharCode(48 + i), {
                     font: font,
@@ -95,6 +97,16 @@ class ThreeDWorld {
                     height: 8
                 })
             }
+            geometrys[10] = new THREE.TextGeometry(':', {
+                font: font,
+                size: 64,
+                height: 8
+            })
+            geometrys[11] = new THREE.TextGeometry('D', {
+                font: font,
+                size: 64,
+                height: 8
+            })
             this.addPartices(geometrys);
         })
     }
@@ -135,12 +147,15 @@ class ThreeDWorld {
         let shaderMaterial = new THREE.ShaderMaterial({
             uniforms:{
                 time: {
-                    type:'f',value: 0.2
+                    type:'f',value: 0.0
                 },
                 process: {
                     type:'f',value: 0.0
                 },
-                number: {
+                current: {
+                    type:'i',value: 0
+                },
+                next: {
                     type:'i',value: 0
                 }
             },
@@ -151,31 +166,74 @@ class ThreeDWorld {
             transparent: true
         });
         var bufferGeometry = new THREE.BufferGeometry();
-        for(var i = 0; i < 10; i++){
-            var vertexs = this.fillMeshToFloat32Array(geometrys[i], 3000);
+        for(var i = 0; i < geometrys.length; i++){
+            console.log("fillMeshToFloat32Array", i);
+            var vertexs = this.fillMeshToFloat32Array(geometrys[i], 1000);
             if(i == 0){
                 bufferGeometry.setAttribute('position', new THREE.BufferAttribute(vertexs, 3));
             }
             bufferGeometry.setAttribute('p' + i, new THREE.BufferAttribute(vertexs, 3));
         }
-        // 创建粒子系统
-        let particleSystem = new THREE.Points(bufferGeometry, shaderMaterial);
-        particleSystem.translateX(-16);
-        particleSystem.translateY(-32);
-        this.scene.add(particleSystem);
-        this.particleSystem = particleSystem;
+        this.numbers = new Array(14);
+        for(let i = 0; i < 8; i++){
+            this.numbers[i] = new THREE.Points(bufferGeometry, shaderMaterial.clone());
+            this.numbers[i].translateY(-64);
+            this.numbers[i].translateX(-48 * (i - 4));
+            this.scene.add(this.numbers[i]);
+        }
+        for(let i = 0; i < 6; i++){
+            this.numbers[i + 8] = new THREE.Points(bufferGeometry, shaderMaterial.clone());
+            this.numbers[i + 8].translateY(24);
+            this.numbers[i + 8].translateX(-48 * (i - 3));
+            this.scene.add(this.numbers[i + 8]);
+        }
     }
     update() {
         this.stats.update();
-        if(this.particleSystem){
-            this.particleSystem.material.uniforms.time.value = Date.now() % 1000 / 1000
-            this.particleSystem.material.uniforms.number.value = Math.floor(Date.now() / 1000) % 10
-            this.particleSystem.material.uniforms.process.value = this.cubic(Date.now() % 1000 / 1000)
+        if(this.numbers){
+            let now = Date.now()
+            let currentSecond = Math.floor(now / 1000)
+            let secondProcess = now % 1000 / 1000
+            let currentNumbers = this.solveNumbers(currentSecond)
+            let nextNumbers = this.solveNumbers(currentSecond + 1)
+            for(let i = 0; i < 14; i++){
+                this.numbers[i].material.uniforms.time.value = secondProcess
+                this.numbers[i].material.uniforms.process.value = this.cubic(secondProcess)
+                if(currentNumbers[i] != null){
+                    this.numbers[i].material.uniforms.current.value = currentNumbers[i]
+                    this.numbers[i].material.uniforms.next.value = nextNumbers[i]
+                }
+            }
         }
         this.renderer.render(this.scene, this.camera);
         requestAnimationFrame(() => {
             this.update()
         });
+    }
+    solveNumbers(second){
+        let day = Math.floor(second / 24 / 60 / 60)
+        let secondOfDay = second % (24 * 60 * 60)
+        console.log(second)
+        let hourOfDay = Math.floor(secondOfDay / 60 / 60)
+        let secondOfHour = secondOfDay % (60 * 60)
+        let minuteOfHour = Math.floor(secondOfHour / 60)
+        let secondOfMinute = secondOfHour % 60
+        let result = new Array(14);
+        result[0] = secondOfMinute % 10
+        result[1] = Math.floor(secondOfMinute / 10)
+        result[2] = 10;
+        result[3] = minuteOfHour % 10
+        result[4] = Math.floor(minuteOfHour / 10)
+        result[5] = 10;
+        result[6] = hourOfDay % 10
+        result[7] = Math.floor(hourOfDay / 10)
+        result[8] = 11;
+        result[13] = Math.floor(day / 10000) % 10
+        result[12] = Math.floor(day / 1000) % 10
+        result[11] = Math.floor(day / 100) % 10
+        result[10] = Math.floor(day / 10) % 10
+        result[9] = day % 10
+        return result;
     }
 }
 
